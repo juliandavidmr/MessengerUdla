f3922f00897b263ed426a7bcecaaa1ddb4752fdd
Julian David <anlijudavid@hotmail.com>
2017-04-27 16:28:05 -0500

    alertas

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from "angularfire2";
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-login',
@@ -13,7 +14,8 @@ export class Login {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public af: AngularFire) {
+    public af: AngularFire,
+    public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -23,8 +25,10 @@ export class Login {
   crear(email, pass) {
     this.af.auth.createUser({ email: email, password: pass }).then(res => {
       console.log("Usuario creado:", res)
+      this.showAlert("Correcto", "El usuario ha sido creado correctamente.")
     }).catch(err => {
       console.log("ERROR: ", err);
+      this.showAlert("Falló", "Por favor verifique su conexión a internet")
     })
   }
 
@@ -39,17 +43,30 @@ export class Login {
       }
     ).then(res => {
       console.log("Usuario logueado:", res)
+      this.showAlert("Correcto", "Logueado correctamente. Ahora puede enviar mensajes.")
     }).catch(err => {
       console.log("ERROR: ", err);
+      this.showAlert("Falló", "Por favor verifique su conexión a internet")
     })
   }
 
   logout() {
     this.af.auth.logout().then(val => {
       console.log(val);
+      this.showAlert("Correcto", "Ha cerrrado la sesión.")
     }).catch(err => {
       console.log("ERROR:", err);
+      this.showAlert("Falló", "Por favor verifique su conexión a internet")
     })
+  }
+
+  showAlert(title: string, desc: string) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: desc,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
